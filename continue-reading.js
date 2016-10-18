9fa5b48c8a4686a37ad3448da3ef16950683bef9
Anthony Garand <garand@garand.me>
2016-10-18 13:34:13 -0400

    Only run ad moving script if on a post with the more tag

--- continue-reading.js
+++ continue-reading.js
@@ -1,25 +1,29 @@
 jQuery(function($) {
 
-	$(".continueReading--button").on( "click", function() {
-		$(".continueReading--hidden").slideToggle();
-		$(this).parent().remove();
-	});
+	if ( $(".continueReading--hidden").length ) {
 
-	var firstWidget = $(".td-ss-main-sidebar").children().first();
+		$(".continueReading--button").on( "click", function() {
+			$(".continueReading--hidden").slideToggle();
+			$(this).parent().remove();
+		});
 
-	continueReadingMoveAd();
+		var firstWidget = $(".td-ss-main-sidebar").children().first();
 
-	$(window).on( 'resize', function() {
 		continueReadingMoveAd();
-	});
 
-	function continueReadingMoveAd() {
-		if ( $(document).width() < 768 ) {
-			firstWidget.insertAfter('.continueReading--hidden');
+		$(window).on( 'resize', function() {
+			continueReadingMoveAd();
+		});
+
+		function continueReadingMoveAd() {
+			if ( $(document).width() < 768 ) {
+				firstWidget.insertAfter('.continueReading--hidden');
+			}
+			else {
+				$(".td-ss-main-sidebar").prepend(firstWidget);	
+			}
 		}
-		else {
-			$(".td-ss-main-sidebar").prepend(firstWidget);	
-		}
+
 	}
 
 });
